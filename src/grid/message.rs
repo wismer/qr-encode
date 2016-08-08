50f43c93ba5d3a53595816304867befaa97e5dc6
Matt Long <matthewhl@gmail.com>
2016-08-08 01:25:13 -0400

    format info masking

--- src/grid/message.rs
+++ src/grid/message.rs
@@ -11,13 +11,13 @@ pub mod message {
         None
     }
 
-
+    struct Generator;
     struct ErrorCorrection(u8);
 
     struct FormatInfo {
-        level_correction: ErrorCorrectionLevel,
+        level: ErrorCorrectionLevel,
         mask: u8,
-        error_correction: Option<u32>
+        error_correction: Option<u16>
     }
 
     fn lcm(x: u8, y: u8) -> u8 {
@@ -35,28 +35,41 @@ pub mod message {
         b
     }
 
+    impl Generator {
+        fn mask_format_info(&self, mask: u16) -> Option<u16> {
+            let gen = 0b10100110111;
+            for n in 0..5 {
+                if mask & (1 << (n + 10)) == 0 {
+                    mask ^= gen << n;
+                }
+            }
+
+            Some(mask)
+        }
+    }
+
     impl FormatInfo {
         fn new(mask: u8, level: ErrorCorrectionLevel) -> FormatInfo {
-            FormatInfo { mask: mask, level_correction: level, error_correction: None }
+            FormatInfo { mask: mask, level: level, error_correction: None }
         }
 
-        fn apply_error_correction(&mut self) {
-            let error_level: u8 = match self.level_correction {
+        fn apply(&mut self) {
+            let error_level: u8 = match self.level {
                 ErrorCorrectionLevel::Low => 3,
                 ErrorCorrectionLevel::Medium => 2,
-                ErrorCorrectionLevel::High => 0,
-                ErrorCorrectionLevel::Quartile => 1
+                ErrorCorrectionLevel::Quartile => 1,
+                ErrorCorrectionLevel::High => 0
             };
+            let format_info = (error_level << 3) ^ self.mask;
 
-            let mask = self.mask;
-
-            let lcm =  // least common multiple of error and mask
+            let generator = Generator;
+            self.error_correction = generator.mask_format_info(format_info as u16);
         }
     }
 
     fn make_format() {
         let mut format_info: FormatInfo = FormatInfo {
-            level_correction: ErrorCorrectionLevel::Low,
+            level: ErrorCorrectionLevel::Low,
             mask: 0b0100,
             error_correction: None
         };
