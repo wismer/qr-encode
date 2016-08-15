2bb1f8b634421dc8e940baa2dbfa861381e38cce
Matt Long <matthewhl@gmail.com>
2016-08-15 15:59:42 -0400

    try different approach

--- qr.py
+++ qr.py
@@ -1,5 +1,6 @@
 from time import sleep
-
+from collections import namedtuple
+from ipdb import set_trace
 # rules:
 # if there are free cells equal or greater than the current block size,
 # then the path is legal. What is expected the cells are placed, return
@@ -104,7 +105,12 @@ class Bit(object):
         return False
 
     def is_valid(self):
-        if self.active or self.is_format or self.is_fixed:
+        if self.active or self.is_format or self.is_fixed or self.is_bridge:
+            return False
+        return True
+
+    def is_free(self):
+        if self.active or self.is_format or self.is_fixed or self.is_bridge:
             return False
         return True
 
@@ -148,37 +154,73 @@ class QR(object):
         bit = self.bits[x][y]
         return bit.is_valid()
 
-    def is_path_valid(self, x, y, path):
-        return all([self.is_bit(x + a, y + b) for a, b in path])
+    def at_fixed_point(self, x, y):
+        try:
+            bit = self.bits[x][y]
+        except IndexError:
+            return False
+        return bit.is_bridge
 
-    def draw_pivots_from(self, x, y, block_size=8):
-        length = block_size
-        if self.is_bit(x, y) and self.bits[x][y].is_bridge:
-            if self.is_bit(x + 1, y):
-                x, y = x + 1, y
-            elif self.is_bit(x - 1, y):
-                x, y = x - 1, y
-            elif self.is_bit(x, y + 1):
-                x, y = x, y + 1
-            elif self.is_bit(x, y - 1):
-                x, y = x, y - 1
-        for path in [UPWARD, TURN_DOWNWARD, DOWNWARD, TURN_UPWARD]:
-            if self.is_path_valid(x, y, path[0:length]):
-                for a, b in path[0:length]:
-                    bit = self.bits[x + a][y + b]
-                    bit.active = True
-                bit.is_fixed = True
-                break
+    def is_cell_valid(self, x, y):
+        if x < 0 or y < 0 or x >= self.size or y >= self.size:
+            return False
+        bit = self.bits[x][y]
+        return bit.is_free()
 
-        if self.is_bit(bit.x - 1, bit.y + 1):
-            return bit.x - 1, bit.y + 1
-        if self.is_bit(bit.x + 1, bit.y + 1):
-            return bit.x + 1, bit.y + 1
+
+    def traverse_upwards(self, x, y, block_size):
+        path = []
+        if not self.is_bit(x, y + 1):
+            floor = y + 1
         else:
-            return (None, None)
+            floor = self.size
+        while len(path) < block_size:
+            if self.is_cell_valid(x + 1, y) and (x + 1, y) not in path:
+                path.append((x, y))
+                if self.is_cell_valid(x, y - 1):
+                    path.append((x, y - 1))
+                x += 1
+            elif not self.is_cell_valid(x - 1, y - 1) and not self.is_cell_valid(x - 1, y):
+                path.append((x, y))
+                y -= 1
+            elif y + 1 == floor and self.is_cell_valid(x, y) and (x, y) not in path:
+                path.append((x, y))
+                if self.is_cell_valid(x, y - 1):
+                    path.append((x, y - 1))
+                    x -= 1
+
+        return path, x, y
+
+    def traverse_downwards(self, x, y, block):
+        path = []
+        floor = y + 1
+        while len(path) < block:
+            if self.is_cell_valid(x - 1, y) and (x - 1, y) not in path:
+                path.append((x, y))
+                if self.is_cell_valid(x, y - 1):
+                    path.append((x, y - 1))
+                x -= 1
+            if not self.is_cell_valid(x + 1, y - 1) and not self.is_cell_valid(x + 1, y):
+                path.append((x, y))
+                y -= 1
+            if y + 1 == floor and self.is_cell_valid(x, y) and (x, y) not in path:
+                path.append((x, y))
+                if self.is_cell_valid(x, y - 1):
+                    path.append((x, y - 1))
+                x += 1
+
+        return path, x, y
+
+    def traverse(self, x, y, blocks, direction='up'):
+        for block in blocks:
+            if not self.is_cell_valid(x - 1, y) and not self.is_cell_valid(x, y + 1):
+                path, x, y = self.traverse_downwards(x, y, block)
+            if not self.is_cell_valid(x + 1, y) and not self.is_cell_valid(x, y + 1):
+                path, x, y = self.traverse_upwards(x, y, block)
+            yield path
 
     def show(self):
-        qr = "".join([" " + str(n) for n in range(0, 21)])
+        qr = "".join([" " + str(n) if n < 10 else " " + str(n - 10) for n in range(0, 21)])
         qr += "\n"
         for (x, bit_row) in enumerate(self.bits):
             for bit in bit_row:
@@ -190,8 +232,11 @@ class QR(object):
 
 qr = QR()
 x, y = 20, 20
-for block in SAMPLE_BLOCKS:
-    if x and y:
-        x, y = qr.draw_pivots_from(x, y, block_size=block)
+blocks = [4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
+for path in qr.traverse(x, y, blocks):
+    for a, b in path:
+        bit = qr.bits[a][b]
+        bit.active = True
+    bit.is_fixed = True
 
-    qr.show()
+qr.show()
