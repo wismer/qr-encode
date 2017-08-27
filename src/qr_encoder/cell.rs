c428e781724d04ba79c632f69e6551969f1407a1
Matt Long <matthewhl@gmail.com>
2017-08-27 12:20:13 -0400

    add operator implementations bc why the heck not?

--- src/qr_encoder/cell.rs
+++ src/qr_encoder/cell.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub, Mul, Div};
+
 #[derive(Copy, Clone, Debug)]
 pub struct Color {
     pub r: u32,
@@ -33,7 +35,7 @@ pub enum CellFlow {
     Available(usize)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Point(pub usize, pub usize);
 
 impl Point {
@@ -44,8 +46,54 @@ impl Point {
     pub fn idx(&self, size: usize) -> usize {
         (self.0 * size) + self.1
     }
+
+    pub fn off_edge(&self, future_position: (isize, isize), size: usize) -> bool {
+        let (fx, fy) = future_position;
+        if self.0 == size && fx == 1 {
+            true
+        } else if self.0 == 0 && fx == -1 {
+            true
+        } else if self.1 == size && fy == 1 {
+            true
+        } else if self.1 == 0 && fy == -1 {
+            true
+        } else {
+            false
+        }
+    }
 }
 
+impl Sub<usize> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: usize) -> Point {
+        Point(self.0, self.1 - rhs)
+    }
+}
+
+impl Add<usize> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: usize) -> Point {
+        Point(self.0, self.1 + rhs)
+    }
+}
+
+impl Mul<usize> for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: usize) -> Point {
+        Point(self.0 + 1, self.1)
+    }
+}
+
+impl Div<usize> for Point {
+    type Output = Point;
+
+    fn div(self, rhs: usize) -> Point {
+        Point(self.0 - 1, self.1)
+    }
+}
 
 #[derive(Debug)]
 pub struct PlotPoint {
